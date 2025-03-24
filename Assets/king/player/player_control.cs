3176669dbcceee8d9e7f4fd65f94303894f71f5c
King <>
2025-03-24 22:39:05 +0900

    ジャンプの機能、それから着地の判定を足しました

--- Assets/king/player/player_control.cs
+++ Assets/king/player/player_control.cs
@@ -13,25 +13,28 @@ public class player_control : MonoBehaviour
 {
     [SerializeField] public player_physical_ability pPA_1;
     [SerializeField] Rigidbody2D playerRb;
+    [SerializeField] player_detectGround pDG;
     public bool isRunning { get; private set; }
+    public bool isJumping { get; private set; }//下のjump関数でtrueにして、着地関数でfalseに(制作予定)
 
     void Update()
     {
          isRunning = run();
+        isJumping = jump();
     }
 
     void FixedUpdate()
     {
-        if (!isRunning)
+        if (!isRunning)//update内でこの処理をしたら不安定だった為Fixed
         {
             playerRb.velocity = new Vector2(0, playerRb.velocity.y);  // 動いていない時は横方向の速度だけゼロにする、また、!isRunningのところに色々足しやすいため、あとあと楽な処理
         }
 
     }
 
-    public bool run()
+    public bool run()//左右の動き、移動中かどうかを返り値として返す
     {
-        int movingDirection = 0;
+        int movingDirection = 0;//向きのX方向を定めます。
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -42,8 +45,32 @@ public class player_control : MonoBehaviour
             movingDirection = 1;
         }
 
-        playerRb.velocity = new Vector2(pPA_1.runSpeed * movingDirection, playerRb.velocity.y);
+        playerRb.velocity = new Vector2(pPA_1.runSpeed * movingDirection, playerRb.velocity.y);//早さにムラができるのは嫌だったのでvelocityを直接書き換えることにしました。
         if (movingDirection == 0) { return false; }
         else { return true; }
     }
+
+    public bool jump()//ジャンプの関数、ジャンプ中かどうかを返り値として返す
+    {
+        if (pDG.isOnGround)//着地中に
+        {
+            if (Input.GetKey(KeyCode.W)) //Wが押されている間ジャンプ、GetKeyDownでも可
+            {
+                playerRb.velocity = new Vector2(playerRb.velocity.x, 0);
+                playerRb.AddForce(new Vector2(0, pPA_1.jumpStrength), ForceMode2D.Impulse);
+                return true;
+            }
+
+            else//着地中にジャンプしてなければそれはジャンプしていないということ
+            {
+                return false;
+            }
+        }
+
+        else
+        {
+            return isJumping;
+        }
+    }
+
 }
